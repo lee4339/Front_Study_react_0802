b43e067e87f9ae60bbfa8c92c62f9cf1569da456
lee4339 <skyho4339@naver.com>
2022-08-13 23:41:13 +0900

    프론트 리액트 0811(배열)

--- react/0809/day2/src/App.js
+++ react/0809/day2/src/App.js
@@ -1,14 +1,33 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import './App.css';
 import UserList2 from './component/userList2';
 // import UserList from './component/userList';
 // import Input from './component/input';
 // import Counter from './component/counter';
+import CreateUser from './component/createUser'
 
 // 상태관리
 
 
 function App() {
+
+  // CreateUser input 태그의 초기값 설정
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+
+  const { username, email } = inputs; //포함하고 의미선언
+  const onChange = (e) => {
+    const {name, value} = e.target
+    setInputs({ 
+      ...inputs,
+      [name]:value
+
+    })
+    //console.log(name)
+  };
+
 
   
   const [users, setUsers] = useState([
@@ -44,6 +63,21 @@ function App() {
     }
   ]);
 
+  const nextId = useRef(6);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,   //현재 커런트
+      username,
+      email
+    }
+    setUsers(users.concat(user))
+    setInputs({
+      username: '',
+      email:''
+    });
+    nextId.current += 1;
+  }
+
   const onRemove = (id) => {
     setUsers(users.filter(user => user.id !== id))
     // user.id가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
@@ -53,11 +87,14 @@ function App() {
     setUsers(users.map(user => user.id === id ? {...user, active : !user.active} : user));    
   }
 
+
   return (
     <div>
       {/* <Counter/> */}
       {/* <Input/> */}
       {/* <UserList/> */}
+
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
       <UserList2 users = {users} onRemove={onRemove} onToggle={onToggle}/>
     </div>
   );
